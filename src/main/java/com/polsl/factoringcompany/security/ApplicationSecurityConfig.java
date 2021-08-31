9378dd254033070636e70fc15af305771d7da1ae
michalgorul <michalgorul@gmail.com>
2021-08-31 15:42:01 +0200

    get users from database

--- src/main/java/com/polsl/factoringcompany/security/ApplicationSecurityConfig.java
+++ src/main/java/com/polsl/factoringcompany/security/ApplicationSecurityConfig.java
@@ -1,16 +1,15 @@
 package com.polsl.factoringcompany.security;
 
+import com.polsl.factoringcompany.security.auth.ApplicationUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.polsl.factoringcompany.security.ApplicationUserRole.ADMIN;
 
@@ -20,6 +19,7 @@ import static com.polsl.factoringcompany.security.ApplicationUserRole.ADMIN;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,15 +35,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService() {
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
 
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("password"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin);
+        return provider;
     }
 }
---
+++ src/main/java/com/polsl/factoringcompany/security/auth/FakeApplicationUserService.java
@@ -0,0 +1,42 @@
+package com.polsl.factoringcompany.security.auth;
+
+import com.google.common.collect.Lists;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.polsl.factoringcompany.security.ApplicationUserRole.ADMIN;
+
+@RequiredArgsConstructor
+@Repository("fake")
+public class FakeApplicationUserService implements ApplicationUserDao{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        ADMIN.getGrantedAuthorities(),
+                        "admin",
+                        passwordEncoder.encode("password"),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
+    }
+}

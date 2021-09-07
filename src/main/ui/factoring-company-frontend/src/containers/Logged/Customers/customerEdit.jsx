a44c1e2ac826fc4c5590a386ade9d85f482a7226
michalgorul <michalgorul@gmail.com>
2021-09-07 14:18:51 +0200

    add customer edit in database

--- src/main/ui/factoring-company-frontend/src/containers/Logged/Customers/customerEdit.jsx
+++ src/main/ui/factoring-company-frontend/src/containers/Logged/Customers/customerEdit.jsx
@@ -4,12 +4,10 @@ import countryList from 'react-select-country-list'
 import { useState, useEffect, useMemo } from "react";
 import { Spinner } from 'react-bootstrap';
 import PhoneInput from 'react-phone-number-input'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Zoom } from 'react-toastify';
 import useFetchWithToken from "../../../services/useFetchWithToken";
 import config from "../../../services/config";
-toast.configure();
+import { updateWithToken } from "../../../services/useUpdateWithToken";
+import { errorToast, infoToast } from "../../../components/toast/makeToast";
 
 const CustomerEdit = () => {
 
@@ -25,18 +23,18 @@ const CustomerEdit = () => {
     const [blacklisted, setBlacklisted] = useState(false);
     const [isPendingN, setIsPendingN] = useState(false);
     const options = useMemo(() => countryList().getData(), [])
-    
+
     const { id } = useParams();
-    const {data: editCustomer, error, isPending} = useFetchWithToken(`${config.API_URL}/api/customer/${id}`);
+    const { data: editCustomer, error, isPending } = useFetchWithToken(`${config.API_URL}/api/customer/${id}`);
     const history = useHistory();
 
     useEffect(() => {
         getCustomer();
-      }, [editCustomer])
+    }, [editCustomer])
 
-      function getCustomer() {
-        if(editCustomer){
-            const countryObject = {value: countryList().getValue(editCustomer.country), label: editCustomer.country };
+    function getCustomer() {
+        if (editCustomer) {
+            const countryObject = { value: countryList().getValue(editCustomer.country), label: editCustomer.country };
             setFirstName(editCustomer.firstName);
             setLastName(editCustomer.lastName)
             setCompanyName(editCustomer.companyName);
@@ -47,116 +45,119 @@ const CustomerEdit = () => {
             setPostalCode(editCustomer.postalCode);
             setPhone(editCustomer.phone);
         }
-            
-      }
+
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setBlacklisted(false);
-        const customer = { firstName, lastName, companyName, country, city, street, postalCode, phone, blacklisted};
+        const customer = { firstName, lastName, companyName, country, city, street, postalCode, phone, blacklisted };
         setIsPendingN(true);
-        const requestOptions = {
-            method: 'PUT',
+        fetch(`${config.API_URL}/api/customer/${id}`, {
+            method: "PUT",
             headers: {
-                'Accept':'application/json',
-                'Content-Type':'application/json'
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
             },
-            body: JSON.stringify(customer)};
-
-        fetch('http://localhost:8000/customers/' + id, requestOptions)
-        .then(() => {
-            setIsPendingN(false);
-            history.push('/user/customers');
+            body: JSON.stringify(customer)
         })
-        .then( () => {
-            toast.info('Customer was updated', {
-                position: "bottom-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                transition: Zoom,
-                className:"text-white bg-primary",
-                });
-        });    
+            .then((response) => {
+                setIsPendingN(false);
+                if (response.ok) {
+                    history.push('/user/customers');
+                    return response;
+                }
+                else {
+                    return response
+                }
+            })
+            .then((response) => {
+                if (response.ok) {
+                    infoToast('Customer was updated')
+                }
+                else {
+                    errorToast('Some of inputs were incorrect')
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
-    
+
     const changeHandler = country => {
         setCountryInList(country)
         setCountry(country.label)
-      }
-    
-    return ( 
+    }
+
+    return (
         <div>
-            {isPending && isPendingN && <div style={{padding: "70px 0", textAlign: "center"}}><Spinner animation="grow" variant="primary" /></div>}
+            {isPending && isPendingN && <div style={{ padding: "70px 0", textAlign: "center" }}><Spinner animation="grow" variant="primary" /></div>}
             {error && <div>{error}</div>}
             {editCustomer && (
-            <div class="container-fluid h-custom">
-                <div class="row d-flex justify-content-start align-items-center">
-                    <div class="col-md-8 col-lg-8 col-xl-6">
-                        <form onSubmit={handleSubmit}>
-                        <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                            <p class="lead fw-normal mt-2 mb-3 display-4">Edit Customer</p>
+                <div class="container-fluid h-custom">
+                    <div class="row d-flex justify-content-start align-items-center">
+                        <div class="col-md-8 col-lg-8 col-xl-6">
+                            <form onSubmit={handleSubmit}>
+                                <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                                    <p class="lead fw-normal mt-2 mb-3 display-4">Edit Customer</p>
 
-                        </div>
-                        <div class="form-outline form-floating mb-3">
-                            <input type="text" class="form-control form-control-lg"
-                            placeholder="Enter a valid email address" required value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                            <label class="form-label">First name</label>
-                        </div>
+                                </div>
+                                <div class="form-outline form-floating mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter a valid email address" required value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                                    <label class="form-label">First name</label>
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                            <input type="text" class="form-control form-control-lg" 
-                            placeholder="Enter password" required value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                            <label class="form-label">Last name</label>
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter password" required value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                                    <label class="form-label">Last name</label>
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                            <input type="text" class="form-control form-control-lg" 
-                            placeholder="Enter password" required value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
-                            <label class="form-label">Company name</label>
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter password" required value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                                    <label class="form-label">Company name</label>
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                        <Select className="" required options={options} value={countryInList} onChange={changeHandler} />
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <Select className="" required options={options} value={countryInList} onChange={changeHandler} />
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                            <input type="text" class="form-control form-control-lg" 
-                            placeholder="Enter password" required value={city} onChange={(e) => setCity(e.target.value)} />
-                            <label class="form-label">City</label>
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter password" required value={city} onChange={(e) => setCity(e.target.value)} />
+                                    <label class="form-label">City</label>
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                            <input type="text" class="form-control form-control-lg" 
-                            placeholder="Enter password" required value={street} onChange={(e) => setStreet(e.target.value)} />
-                            <label class="form-label">Street</label>
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter password" required value={street} onChange={(e) => setStreet(e.target.value)} />
+                                    <label class="form-label">Street</label>
+                                </div>
 
-                        <div class="form-floating form-outline mb-3">
-                            <input type="text" class="form-control form-control-lg" 
-                            placeholder="Enter password" required value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
-                            <label class="form-label">Postal code</label>
-                        </div>
+                                <div class="form-floating form-outline mb-3">
+                                    <input type="text" class="form-control form-control-lg"
+                                        placeholder="Enter password" required value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
+                                    <label class="form-label">Postal code</label>
+                                </div>
 
-                        <div class="form-floating form-outline form-control form-control-lg mb-3">
-                            <PhoneInput class="form-control form-control-lg"
-                            placeholder="Phone number" required value={phone} onChange={setPhone}/>
+                                <div class="form-floating form-outline form-control form-control-lg mb-3">
+                                    <PhoneInput class="form-control form-control-lg"
+                                        placeholder="Phone number" required value={phone} onChange={setPhone} />
+                                </div>
+                                <div class="mb-3">
+                                    {!isPending && <button class="btn btn-primary rounded-pill btn-lg">Edit Customer</button>}
+                                    {isPending && <button class="btn btn-primary rounded-pill btn-lg" disabled>Editing customer...</button>}
+                                </div>
+
+                            </form>
                         </div>
-                        <div class="mb-3">
-                            {!isPending && <button class="btn btn-primary rounded-pill btn-lg">Edit Customer</button>}
-                            {isPending && <button class="btn btn-primary rounded-pill btn-lg" disabled>Editing customer...</button>}
-                        </div>
-                            
-                        </form>
                     </div>
-                </div>
-            </div>)}
+                </div>)}
         </div>
-        
-     );
+
+    );
 }
- 
+
 export default CustomerEdit;

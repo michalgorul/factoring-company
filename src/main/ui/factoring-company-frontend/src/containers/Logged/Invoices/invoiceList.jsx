6bb310c5caa96b4e009603aa4c7f88fdf73a3909
michalgorul <michalgorul@gmail.com>
2021-09-08 12:59:49 +0200

    add filtering invoices

--- src/main/ui/factoring-company-frontend/src/containers/Logged/Invoices/invoiceList.jsx
+++ src/main/ui/factoring-company-frontend/src/containers/Logged/Invoices/invoiceList.jsx
@@ -1,13 +1,24 @@
 import Table from 'react-bootstrap/Table'
-import {Spinner} from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 import useFetchWithToken from '../../../services/useFetchWithToken';
 import config from '../../../services/config';
+import { useEffect } from 'react';
 
 
-const InvoiceList = ({whatInvoices}) => {
+const InvoiceList = ({ whatInvoices }) => {
     const { error, isPending, data: invoices } = useFetchWithToken(`${config.API_URL}/api/invoice`)
 
-    return ( 
+    useEffect(() => {
+        if (invoices) {
+            invoices.forEach(invoice=>{
+                invoice.creationDate = new Date(invoice.creationDate).toDateString();
+                invoice.paymentDeadline = new Date(invoice.paymentDeadline).toDateString();
+            })
+        }
+
+    }, [invoices]);
+
+    return (
         <Table striped borderless hover>
             <caption>List of {whatInvoices} invoices</caption>
             <thead>
@@ -21,22 +32,24 @@ const InvoiceList = ({whatInvoices}) => {
                 </tr>
             </thead>
             <tbody>
-            { error &&<> <div class="alert alert-warning fs-3" role="alert">{error} </div> 
-                            <a class="text-decoration-none ms-3 fs-3" href="" onClick={() => {window.location.href="/something"}}> Click to refresh </a></>}
-            { isPending && <div style={{padding: "70px 0", textAlign: "center"}}><Spinner animation="grow" variant="primary" /></div> }
-            { invoices && invoices.map(invoice => (
-                    <tr key={invoice.id} className="clickable" onclick="#">
-                        <th>{invoice.id}</th>
-                        <td><a href={"/user/invoices/" + invoice.id} className="text-decoration-none">{invoice.invoiceNumber}</a></td>
-                        <td>{invoice.creationDate}</td>
-                        <td>{invoice.paymentDeadline}</td>
-                        <td>{invoice.toPay}</td>
-                        <td>{invoice.paid}</td>
-                    </tr>
-            ))}
+                {error && <> <div class="alert alert-warning fs-3" role="alert">{error} </div>
+                    <a class="text-decoration-none ms-3 fs-3" href="" onClick={() => { window.location.href = "/something" }}> Click to refresh </a></>}
+                {isPending && <div style={{ padding: "70px 0", textAlign: "center" }}><Spinner animation="grow" variant="primary" /></div>}
+                {invoices && invoices
+                    .filter(invoice => invoice.status == whatInvoices)
+                    .map(invoice => (
+                        <tr key={invoice.id} className="clickable" onclick="#">
+                            <th>{invoice.id}</th>
+                            <td><a href={"/user/invoices/" + invoice.id} className="text-decoration-none">{invoice.invoiceNumber}</a></td>
+                            <td>{invoice.creationDate}</td>
+                            <td>{invoice.paymentDeadline}</td>
+                            <td>{invoice.toPay}</td>
+                            <td>{invoice.paid}</td>
+                        </tr>
+                    ))}
             </tbody>
         </Table>
-     );
+    );
 }
- 
+
 export default InvoiceList;

4b98378cba860623640b4123859dd9631781104d
michalgorul <michalgorul@gmail.com>
2021-11-01 01:08:24 +0100

    make updating payment info on invoice more life like

---
+++ src/main/java/com/polsl/factoringcompany/invoice/InvoicePaymentInfoUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.polsl.factoringcompany.invoice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class InvoicePaymentCurrencyUpdateRequest {
+    private String currencyName;
+    private String paymentTypeName;
+}

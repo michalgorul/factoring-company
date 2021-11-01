db9eedb18416f27081e44d919a96fbdc794d2879
michalgorul <michalgorul@gmail.com>
2021-11-01 17:40:16 +0100

    make fetching from database in credit details

--- src/main/ui/factoring-company-frontend/src/containers/Logged/Credit/creditDetails.jsx
+++ src/main/ui/factoring-company-frontend/src/containers/Logged/Credit/creditDetails.jsx
@@ -3,37 +3,39 @@ import { useParams } from "react-router-dom";
 import { Spinner } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import config from "../../../services/config";
+import useFetchWithToken from "../../../services/useFetchWithToken";
 toast.configure();
 
 const CreditDetails = () => {
-    const { id } = useParams();
-    const {data: credit, errorC, isPendingC} = useFetch('http://localhost:8000/credits-funded/' + id);
+	const { id } = useParams();
+	const { data: credit, errorC, isPendingC } = useFetchWithToken(`${config.API_URL}/api/credit/${id}`);
 
 
-    return ( 
-        <div className="">
-        {isPendingC && <div style={{padding: "70px 0", textAlign: "center"}}><Spinner animation="grow" variant="primary" /></div>}
-        {errorC && <div>{errorC}</div>}
-        {credit && (
-            <article class="mt-2 ms-3">
-                <div class="media align-items-center py-1">
-              <div class="media-body ml-4">
-                <h4 class="display-3">Credit details</h4>
-              </div>
-            </div>
-                <h5 class="mt-4 mb-3">General</h5>
-                <div class="container">
-					<div class="row align-items-start ms-2">
-						<div class="col-3">
-							<ul class="list-group list-group-flush">
-								<li class="list-group-item list-group-item-action fw-bold">Remaining to be paid:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Loan amount:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Next payment:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Number of remaining installments:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Account balance to be paid:</li>
-							</ul>
+	return (
+		<div className="">
+			{isPendingC && <div style={{ padding: "70px 0", textAlign: "center" }}><Spinner animation="grow" variant="primary" /></div>}
+			{errorC && <div>{errorC}</div>}
+			{credit && (
+				<article class="mt-2 ms-3">
+					<div class="media align-items-center py-1">
+						<div class="media-body ml-4">
+							<h4 class="display-3">Credit details</h4>
 						</div>
-							<div class="col-3">
+					</div>
+					<h5 class="mt-4 mb-3">General</h5>
+					<div class="container">
+						<div class="row align-items-start ms-2">
+							<div class="col-6 col-lg-3">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item list-group-item-action fw-bold">Remaining to be paid:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Loan amount:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Next payment:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Number of remaining installments:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Account balance to be paid:</li>
+								</ul>
+							</div>
+							<div class="col-6 col-lg-3">
 								<ul class="list-group list-group-flush">
 									<li class="list-group-item list-group-item-action">{credit.leftToPay}</li>
 									<li class="list-group-item list-group-item-action">{credit.amount}</li>
@@ -42,22 +44,22 @@ const CreditDetails = () => {
 									<li class="list-group-item list-group-item-action">{credit.balance}</li>
 								</ul>
 							</div>
-					</div>
-				</div>
-              
-                
-				<h5 class="mt-4 mb-3">Other</h5>
-				<div class="container">
-					<div class="row align-items-start ms-2">
-						<div class="col-3">
-							<ul class="list-group list-group-flush">
-							    <li class="list-group-item list-group-item-action fw-bold">Rate of interest:</li>
-							    <li class="list-group-item list-group-item-action fw-bold">Next payment date:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Creation Date:</li>
-								<li class="list-group-item list-group-item-action fw-bold">Last installment Date:</li>
-							</ul>
 						</div>
-							<div class="col-3">
+					</div>
+
+
+					<h5 class="mt-4 mb-3">Other</h5>
+					<div class="container">
+						<div class="row align-items-start ms-2">
+							<div class="col-6 col-lg-3">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item list-group-item-action fw-bold">Rate of interest:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Next payment date:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Creation Date:</li>
+									<li class="list-group-item list-group-item-action fw-bold">Last installment Date:</li>
+								</ul>
+							</div>
+							<div class="col-6 col-lg-3">
 								<ul class="list-group list-group-flush mb-3">
 									<li class="list-group-item list-group-item-action">{credit.rateOfInterest + '%'}</li>
 									<li class="list-group-item list-group-item-action">{credit.nextPaymentDate}</li>
@@ -67,16 +69,25 @@ const CreditDetails = () => {
 							</div>
 						</div>
 					</div>
-                
 
-                <div class="alert clearfix mt-2">
-                    <button class="btn btn-lg mb-3 btn-primary rounded-pill float-center me-3">Overpay the loan</button>
-                    <button class="btn btn-lg mb-3 btn-primary rounded-pill float-center">Requests</button>
-                </div>
-            </article> 
-        )}
-    </div>
-     );
+
+					<div className="container mb-4">
+						<div className="row align-items-start">
+							<div class="mt-3 col-12 col-lg-3 text-center">
+								<button class="btn btn-lg mb-3 btn-primary rounded-pill me-3">Overpay the loan</button>
+
+							</div>
+
+							<div class="mt-3 col-12 col-lg-3 text-center">
+								<button class="btn btn-lg mb-3 btn-primary rounded-pill">Requests</button>
+
+							</div>
+						</div>
+					</div>
+				</article>
+			)}
+		</div>
+	);
 }
- 
+
 export default CreditDetails;

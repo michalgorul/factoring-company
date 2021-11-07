8cf2dccbe1fd996ea3db63990959e29f49ff1a1e
michalgorul <michalgorul@gmail.com>
2021-11-07 18:02:27 +0100

    add stretched-link on customers list

--- src/main/ui/factoring-company-frontend/src/containers/Logged/Customers/customerList.jsx
+++ src/main/ui/factoring-company-frontend/src/containers/Logged/Customers/customerList.jsx
@@ -5,7 +5,7 @@ const CustomerList = ({customers}) => {
             <li key={customer.id} class="list-group-item list-group-item list-group-item-action d-flex align-items-start">
             <div class="ms-2 me-auto fs-5">
             <div class="fw-bold">
-                <a href={'/user/customers/'+ customer.id} class="text-decoration-none">{customer.firstName + ' ' + customer.lastName}</a>    
+                <a href={'/user/customers/'+ customer.id} class="text-decoration-none stretched-link">{customer.firstName + ' ' + customer.lastName}</a>    
             </div>
             {customer.companyName}
             </div>

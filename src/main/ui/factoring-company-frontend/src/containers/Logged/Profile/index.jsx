4aba079ff3160c495ecc2737add02e050b51eb7f
michalgorul <michalgorul@gmail.com>
2021-11-07 13:58:19 +0100

    change buttons to links in editing profile info

--- src/main/ui/factoring-company-frontend/src/containers/Logged/Profile/index.jsx
+++ src/main/ui/factoring-company-frontend/src/containers/Logged/Profile/index.jsx
@@ -104,41 +104,39 @@ const Profile = () => {
 		}
 	}
 
-	const showEditButtons = (bank, company) => {
+	const showEditBankAccount = (bank, company) => {
 		if (bank != null && company != null) {
 			return (
-				<div class="align-items-center mt-3 ms-5">
-					<a href={"/user/profile/edit"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your profile</a>
-					<a href={"/user/profile/company/edit/" + company.id} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your company</a>
-					<a href={"/user/bank-account/edit/" + bank.id} class="btn btn-lg mb-3 btn-primary rounded-pill ">Edit your bank account</a>
-				</div>
+				<a href={"/user/bank-account/edit/" + bank.id} className="text-decoration-none ml-auto h6">Edit your bank account</a>
 			)
 		}
-		else if (bank != null && company == null){
+		else if (bank != null && company == null) {
 			return (
-				<div class="align-items-center mt-3 ms-5">
-					<a href={"/user/profile/edit"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your profile</a>
-					<a href={"/user/profile/company/create"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Add your company</a>
-					<a href={"/user/bank-account/edit/" + bank.id} class="btn btn-lg mb-3 btn-primary rounded-pill disabled">Add your bank account</a>
-				</div>
+				<a href={"/user/profile/bank/create"} className="text-decoration-none ml-auto h6 disabled">Add your bank account</a>
 			)
 		}
-		else if (bank == null && company != null){
+		else if (bank == null && company != null) {
 			return (
-				<div class="align-items-center mt-3 ms-5">
-					<a href={"/user/profile/edit"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your profile</a>
-					<a href={"/user/profile/company/edit/" + company.id} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your company</a>
-					<a href={"/user/profile/bank/create"} class="btn btn-lg mb-3 btn-primary rounded-pill">Add your bank account</a>
-				</div>
+				<a href={"/user/profile/bank/create"} className="text-decoration-none ml-auto h6">Add your bank account</a>
 			)
 		}
-		else{
+		else {
 			return (
-				<div class="align-items-center mt-3 ms-5">
-					<a href={"/user/profile/edit"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Edit your profile</a>
-					<a href={"/user/profile/company/create"} class="btn btn-lg mb-3 btn-primary rounded-pill me-4">Add your company</a>
-					<a href={"/user/profile/bank/create"} class="btn btn-lg mb-3 btn-primary rounded-pill disabled">Add your bank account</a>
-				</div>
+				<a href={"/user/profile/bank/create"} className="text-decoration-none ml-auto h6">Add your bank account</a>
+			)
+		}
+
+	}
+
+	const showEditCompany = ( company) => {
+		if (company != null) {
+			return (
+				<a href={"/user/profile/company/edit/" + company.id} class="text-decoration-none ml-auto h6">Edit your company</a>
+			)
+		}
+		else {
+			return (
+				<a href={"/user/profile/company/create"} class="text-decoration-none ml-auto h6">Add your company</a>
 			)
 		}
 
@@ -147,7 +145,6 @@ const Profile = () => {
 
 	return (
 		<>
-
 			{isPendingU && <div style={{ padding: "70px 0", textAlign: "center" }}><Spinner animation="grow" variant="primary" /></div>}
 			{errorU && <div>{errorU}</div>}
 
@@ -166,7 +163,7 @@ const Profile = () => {
 					<h5 class="mt-4 mb-3">Address & Phone</h5>
 					<div class="container">
 						<div class="row align-items-start ms-3">
-							<div class="col-3">
+							<div class="col-5 col-lg-3">
 								<ul class="list-group list-group-flush">
 									<li class="list-group-item list-group-item-action fw-bold">Country:</li>
 									<li class="list-group-item list-group-item-action fw-bold">City:</li>
@@ -175,7 +172,7 @@ const Profile = () => {
 									<li class="list-group-item list-group-item-action fw-bold">Phone number:</li>
 								</ul>
 							</div>
-							<div class="col-5">
+							<div class="col-7 col-lg-4">
 								<ul class="list-group list-group-flush">
 									<li class="list-group-item list-group-item-action">{user.country}</li>
 									<li class="list-group-item list-group-item-action">{user.city}</li>
@@ -187,6 +184,12 @@ const Profile = () => {
 						</div>
 					</div>
 
+					<div class="d-flex mb-4 col-12 col-lg-7 mt-3">
+						<a href={"/user/profile/edit"} className="text-decoration-none ml-auto h6">Edit your profile</a>
+					</div>
+
+
+
 					<h5 class="mt-4 mb-3">Bank account</h5>
 					<div class="container">
 						<div class="row align-items-start ms-3">
@@ -197,10 +200,14 @@ const Profile = () => {
 									<li class="list-group-item list-group-item-action fw-bold">SWIFT:</li>
 								</ul>
 							</div>
-							<div class="col-5">
+							<div class="col-4">
 								{showBankAccountDetails(bank)}
 							</div>
 						</div>
+					</div>
+
+					<div class="d-flex mb-4 col-12 col-lg-7 mt-3">
+						{showEditBankAccount(bank, company)}
 					</div>
 
 
@@ -223,7 +230,9 @@ const Profile = () => {
 							</div>
 						</div>
 					</div>
-					{showEditButtons(bank, company)}
+					<div class="d-flex mb-4 col-12 col-lg-7 mt-3">
+						{showEditCompany(bank, company)}
+					</div>
 
 				</div>)}
 		</>

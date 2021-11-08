d8e7f233c39193905c0f7c91117ef91d7761dca3
michalgorul <michalgorul@gmail.com>
2021-11-08 10:06:36 +0100

    remove unused seller entity

--- src/main/java/com/polsl/factoringcompany/seller/SellerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.polsl.factoringcompany.seller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SellerRepository extends JpaRepository<SellerEntity, Long> {
-}
